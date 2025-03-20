099d8b7251ccf2245bdacae822fd8d8b0d231e51
caplex <caplex.work@email.com>
2025-03-20 17:16:22 +0700

    Добавил сервисы для обработки даных о: - Пользователе - Фильмах Дальнейшие методы будут добавляться по мере разработки фич.

---
+++ src/services/user.service.ts
@@ -0,0 +1,28 @@
+import { UserData } from './../types/User';
+
+const userService: UserService = {
+  getUserInfo(id: string): Promise<UserData> {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        try {
+          const data = localStorage.getItem('user');
+          if (!data) return null;
+
+          const user = JSON.parse(data).find((u: UserData) => u._id === id);
+          if (!user) return null;
+
+          res(user);
+        }
+        catch (err) {
+          rej(err);
+        }
+      })
+    })
+  }
+};
+
+export type UserService = {
+  getUserInfo: (id: string) => Promise<UserData>;
+}
+
+export default userService;
