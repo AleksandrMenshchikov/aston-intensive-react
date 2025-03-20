38b39bcbffab30d36f46676b710f96560942d9f4
caplex <caplex.work@email.com>
2025-03-20 17:16:22 +0700

    Добавил сервисы для обработки даных о: - Пользователе - Фильмах Дальнейшие методы будут добавляться по мере разработки фич.

---
+++ src/services/film.service.ts
@@ -0,0 +1,30 @@
+import { Film } from "../types/Film";
+
+const X_RAPID_KEY = 'c40f5dc7ffmsh784d0dc2efd0961p1866b9jsn9aa4867a76dd';
+const API_HOST = 'moviesdatabase.p.rapidapi.com';
+//TODO заменить импортом из констант после мерджа
+
+const filmService = {
+  async getTopRatedSeries(): Promise<Film[] | undefined>  {
+    const LIST_NAME = 'top_rated_series_250';
+    const url = `https://${API_HOST}/titles/random?list=${LIST_NAME}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': X_RAPID_KEY,
+        'x-rapidapi-host': API_HOST,
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const textResult = await response.text();
+      const result = JSON.parse(textResult);
+      return result.results;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+export default filmService;
---
+++ src/types/User.ts
@@ -0,0 +1,6 @@
+export type UserData = {
+  _id: string;
+  name: string;
+  favorites: string[];
+  history: string[];
+}
