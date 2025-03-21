96a8d0daf4608f7e579ee69f57b43e22f5aed698
Caplex <caplex.work@gmail.com>
2025-03-21 15:42:29 +0700

    Backup save

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,16 +3,11 @@ import { Route, Routes } from 'react-router';
 import { Layout } from './Layout';
 import { Home } from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
-import filmService from '../services/film.service';
-import fakeServer from '../backend/api/fakeServer';
 
 const Signin = lazy(() => import('../pages/Signin'));
 const Signup = lazy(() => import('../pages/Signup'));
 
 export function App() {
-  filmService.getTopRatedSeries();
-  fakeServer.updateUser('asda');
-
   return (
     <Routes>
       <Route element={<Layout />}>
