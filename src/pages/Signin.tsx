6c97d4c19f8be9cb26be7c3e58b05eeb245b35b9
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2025-03-25 17:23:26 +0300

    Рефакторинг

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,30 +1,5 @@
 import React from 'react';
-import { signin, selectUser, setError } from '../redux/slices/userSlice';
-import { useAppDispatch, useAppSelector } from '../redux/store';
-import { Container } from '../components/Container';
-import { emailPattern } from '../utils/emailPattern';
-import { FormTitle } from '../components/FormTitle';
-import { Form } from '../components/Form';
 
 export default function Signin() {
-  const { email, password } = useAppSelector(selectUser);
-  const dispatch = useAppDispatch();
-
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
-
-    if (!email.match(emailPattern) || !password) {
-      dispatch(setError('Пожалуйста, заполните все поля'));
-      return;
-    }
-
-    dispatch(signin({ email, password }));
-  }
-
-  return (
-    <Container>
-      <FormTitle text="Вход" />
-      <Form onHandleSubmit={handleSubmit} buttonText="Войти" />
-    </Container>
-  );
+  return <h1>Signin</h1>;
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import { signup, selectUser, setError } from '../redux/slices/userSlice';
-import { useAppDispatch, useAppSelector } from '../redux/store';
-import { emailPattern } from '../utils/emailPattern';
-
-import { Container } from '../components/Container';
-import { FormTitle } from '../components/FormTitle';
-import { Form } from '../components/Form';
-
+import SignUpForm from '../components/SignUpForm';
 export default function Signup() {
-  const { email, password } = useAppSelector(selectUser);
-  const dispatch = useAppDispatch();
-
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
-
-    if (!email.match(emailPattern) || !password) {
-      dispatch(setError('Пожалуйста, заполните все поля'));
-      return;
-    }
-
-    dispatch(signup({ email, password }));
-  }
-
   return (
-    <Container>
-      <FormTitle text="Регистрация" />
-      <Form onHandleSubmit={handleSubmit} buttonText="Зарегистрироваться" />
-    </Container>
+    <>
+      <h1>Signup</h1>
+      <SignUpForm />
+    </>
   );
 }
