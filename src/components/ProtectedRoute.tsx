7227c9ddb2806275f3c7c52f6110d49229c22a85
caplex <caplex.work@email.com>
2025-03-26 10:31:54 +0700

    pre-commit formating

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,19 @@
 import React, { ReactElement } from 'react';
 import { Navigate, Outlet } from 'react-router';
 
-export default function ProtectedRoute({ isAuthenticated, redirectPath = '/signin' }: Props) {
-  return isAuthenticated ? <Outlet /> : <Navigate replace state={{ from: location.pathname }} to={redirectPath} />;
-};
+export default function ProtectedRoute({
+  isAuthenticated,
+  redirectPath = '/signin',
+}: Props) {
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate replace state={{ from: location.pathname }} to={redirectPath} />
+  );
+}
 
 type Props = {
-  isAuthenticated: boolean,
-  redirectPath?: string
+  isAuthenticated: boolean;
+  redirectPath?: string;
   children: ReactElement;
 };
