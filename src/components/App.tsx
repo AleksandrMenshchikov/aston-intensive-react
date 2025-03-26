5632a762c22d46baf46576871947355adca7a414
Caplex <165675467+CaplexW@users.noreply.github.com>
2025-03-26 13:03:59 +0700

    Убрал защиту с Search

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,28 +4,19 @@ import { Layout } from './Layout';
 import { Home } from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
 import { Page } from '../types/enums';
-import ProtectedRoute from './ProtectedRoute';
 
 const Signin = lazy(() => import('../pages/Signin'));
 const Signup = lazy(() => import('../pages/Signup'));
 const Search = lazy(() => import('../pages/Search'));
 
 export function App() {
-  const isLogged = false; //TODO Заглушка. Заменить на селектор после мержда.
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index path={Page.Home} element={<Home />} />
         <Route path={Page.Signin} element={<Signin />} />
         <Route path={Page.Signup} element={<Signup />} />
-        <Route
-          path={Page.Search}
-          element={
-            <ProtectedRoute isAuthenticated={isLogged}>
-              <Search />
-            </ProtectedRoute>
-          }
-        />
+        <Route path={Page.Search} element={<Search />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
